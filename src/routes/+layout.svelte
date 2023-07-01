773f954ee7908516958425f55b8cdf69c74e3fa4
David Woolf <woolf.david@gmail.com>
2023-07-01 08:45:26 -0700

    laying out the homepage

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -2,6 +2,16 @@
 	import '../app.css';
 </script>
 
+<header class="flex items-center h-20 px-12 justify-between bg-white">
+	<h1 class="font-semibold text-lg">David Woolf</h1>
+
+	<nav>
+		<a class="underline hover:no-underline" href="https://github.com/davidwoolf" target="_blank"
+			>Github</a
+		>
+	</nav>
+</header>
+
 <slot />
 
 <style lang="postcss">
