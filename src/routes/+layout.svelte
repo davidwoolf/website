be6f3a6a31952e585eb624566c02366ff3336416
David Woolf <woolf.david@gmail.com>
2023-07-01 08:39:47 -0700

    wired up tailwindcss

---
+++ src/routes/+layout.svelte
@@ -0,0 +1,11 @@
+<script lang="ts">
+	import '../app.css';
+</script>
+
+<slot />
+
+<style lang="postcss">
+	:global(body) {
+		-webkit-font-smoothing: antialiased;
+	}
+</style>
---
+++ tailwind.config.cjs
@@ -0,0 +1,18 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+	content: ['./src/**/*.{html,js,svelte,ts}'],
+	theme: {
+		extend: {
+			fontSize: {
+				hint: '0.625rem',
+				xs: '0.75rem',
+				sm: '0.8125rem',
+				base: '0.9375rem',
+				lg: '1.125rem',
+				xl: '1.5rem',
+				'2xl': '2rem'
+			}
+		}
+	},
+	plugins: []
+};
