879bba89477843c5949650eee9dc5e475117c04f
David Woolf <woolf.david@gmail.com>
2023-07-25 08:53:29 -0700

    reverted layout updates to the homepage due to in-progress work being ommitted

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -2,28 +2,26 @@
   import { assets } from "$app/paths";
   import Heading1 from "@components/text/heading1.svelte";
   import Image from "@components/home/image.svelte";
-  import History from "@components/home/history.svelte";
 </script>
 
-<div class="p-6 md:py-24 md:px-16">
-  <div class="pb-48 pt-12 flex flex-col-reverse lg:grid lg:grid-cols-2 items-center">
-    <div class="text-center lg:text-left">
-      <Heading1>
-        Software engineer
-        <span class="align-middle text-lg font-normal px-1 opacity-20">|</span> web
-      </Heading1>
+<div
+  class="page-container p-6 md:py-24 md:px-16 flex flex-col-reverse lg:grid lg:grid-cols-2 items-center">
+  <div class="text-center lg:text-left">
+    <Heading1>
+      Software engineer
+      <span class="align-middle text-lg font-normal px-1 opacity-20">|</span> web
+    </Heading1>
 
-      <p class="pt-2 text-base md:text-lg font-regular text-gray-900/60 max-w-lg">
-        I'm David Woolf, a front-end software engineer, building products professionally
-        for over 10 years. Currently living in San Diego, California with
-        my&nbsp;dog&nbsp;Luka.
-      </p>
-    </div>
+    <p class="pt-2 text-base md:text-lg font-regular text-gray-900/60 max-w-lg">
+      I'm David Woolf, a front-end software engineer, building products professionally for
+      over 10 years. Currently living in San Diego, California with my&nbsp;dog&nbsp;Luka.
+    </p>
+  </div>
 
-    <div
-      class="origin-top scale-50 translate-y-8 md:origin-center md:scale-75 md:-translate-y-16 lg:translate-y-0 xl:scale-100 grid grid-cols-1 grid-rows-1 place-items-center group">
-      <Image
-        classes="
+  <div
+    class="origin-top scale-50 translate-y-8 md:origin-center md:scale-75 md:-translate-y-16 lg:translate-y-0 xl:scale-100 grid grid-cols-1 grid-rows-1 place-items-center group">
+    <Image
+      classes="
           w-48
           translate-x-32
           translate-y-0
@@ -31,11 +29,11 @@
           group-hover:-translate-y-12
           group-hover:scale-110
         "
-        src="{assets}/luka-2.jpeg"
-        alt="Luka in nature" />
+      src="{assets}/luka-2.jpeg"
+      alt="Luka in nature" />
 
-      <Image
-        classes="
+    <Image
+      classes="
           w-40
           transform
           -translate-x-2
@@ -44,11 +42,11 @@
           group-hover:translate-y-32
           group-hover:scale-110
         "
-        src="{assets}/luka.jpeg"
-        alt="My dog Luka" />
+      src="{assets}/luka.jpeg"
+      alt="My dog Luka" />
 
-      <Image
-        classes=" 
+    <Image
+      classes=" 
           w-56
           transform
           -translate-x-32
@@ -57,12 +55,9 @@
           group-hover:-translate-y-20
           group-hover:scale-90
         "
-        src="{assets}/profile.jpeg"
-        alt="David Woolf" />
-    </div>
+      src="{assets}/profile.jpeg"
+      alt="David Woolf" />
   </div>
-
-  <!-- <History /> -->
 </div>
 
 <svelte:head>
